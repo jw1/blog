659a954d455989e2da0f70bae6a55f7f9391f27c
James Warlick <jwarlick@financialpayments.com>
2017-07-28 13:41:32 -0500

    finish the intro guide

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "jimmy", password: "jimmy", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
